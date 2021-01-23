b01d3947ca048f8f7cc065a0c38f25d88a38e3f5
cuong71 <63961685+cuong71@users.noreply.github.com>
2021-01-23 09:25:54 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,8 @@ let steps = 0
 basic.showNumber(0)
 basic.forever(function () {
     if (input.acceleration(Dimension.Strength) > 1500) {
+        let Schritte = 0
         steps += 1
-        basic.showNumber(steps)
+        basic.showNumber(Schritte)
     }
 })
